50d16b10e212550ba2daff003bbda6ea29544c58
Bruno Martins <brunoseivam@gmail.com>
2016-10-02 17:52:41 -0400

    Add logic to create and edit Puzzles

--- demonstration.py
+++ demonstration.py
@@ -21,12 +21,53 @@ from demo import models  # noqa
 
 User = models.User
 Clue = models.Clue
+Puzzle = models.Puzzle
+PuzzleClue = models.PuzzleClue
 
 path = partial(os.path.join, os.path.dirname(__file__))
 path.__doc__ = 'Get the full path relative to the directory of this file.'
 
 from_thread = partial(get_event_loop().run_in_executor, None)
 from_thread.__doc__ = 'Run a function in another thread.'
+
+def words_overlap(word1, word2):
+    """Determines if two words overlap."""
+    first1, last1 = word1
+    first2, last2 = word2
+
+    return ((first1 >= first2 and first1 <= last2) or
+           (last1 >= first2 and last1 <= last2))
+
+def clue_fits(puzzle, clue, row, col, direction):
+    """Determines if a clue fits inside a puzzle."""
+    line_len = puzzle.nrows if direction else puzzle.ncols
+    first = row if direction else col
+    last = first + len(clue.answer) -1
+
+    # Check if it fits inside puzzle
+    if last >= line_len:
+        return False
+
+    # Check if it doesn't overlap with another clue
+    for p_clue in puzzle.clues:
+        p_clue_first = p_clue.row if p_clue.direction else p_clue.col
+        p_clue_last = p_clue_first + len(p_clue.clue.answer) - 1
+        if p_clue.direction == direction:
+            if (direction and p_clue.col == col) or (not direction and p_clue.row == row):
+                if words_overlap((first,last),(p_clue_first,p_clue_last)):
+                    return False
+
+    # Check if letters in the word match the other words already in the puzzle
+    matrix = puzzle.as_matrix()
+    for letter in clue.answer:
+        if matrix[row][col] is not None and matrix[row][col] != letter:
+            return False
+        if direction:
+            row += 1
+        else:
+            col += 1
+
+    return True
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -138,6 +179,65 @@ class CreateClueHandler(BaseHandler):
             self.session.add(Clue(clue=clue, answer=answer))
         self.render('create_clue.html', error='Clue created')
 
+class CreatePuzzleHandler(BaseHandler):
+    """Puzzle creation handler."""
+
+    def get(self):
+        """Render /puzzle/create/."""
+        self.render('create_puzzle.html', error=None)
+
+    def post(self):
+        """Method for creating a puzzle."""
+        name = self.get_argument('name')
+        nrows = self.get_argument('nrows')
+        ncols = self.get_argument('ncols')
+        with self.session.begin():
+            puzzle = Puzzle(name=name, nrows=nrows, ncols=ncols)
+            self.session.add(puzzle)
+        self.redirect('/puzzle/edit/{}'.format(puzzle.name))
+
+class EditPuzzleHandler(BaseHandler):
+    """Puzzle edition handler."""
+
+    def get(self, name):
+        """Render /puzzle/edit/{name}."""
+        with self.session.begin():
+            puzzle = self.session.query(Puzzle).filter_by(name=name).first()
+            all_clues = self.session.query(Clue).all()
+        if puzzle is None:
+            raise tornado.web.HTTPError(404)
+
+        used_clues = [c.clue for c in puzzle.clues]
+        available_clues = [c for c in all_clues if c not in used_clues]
+        self.render('edit_puzzle.html', puzzle=puzzle, clues=available_clues, error=None)
+
+    def post(self, name):
+        """Method for editing a puzzle."""
+        clue = self.get_argument('clue')
+        row = int(self.get_argument('row'))
+        col = int(self.get_argument('col'))
+        direction = bool(int(self.get_argument('direction')))
+        with self.session.begin():
+            puzzle = self.session.query(Puzzle).filter_by(name=name).first()
+            all_clues = self.session.query(Clue).all()
+            clue = self.session.query(Clue).filter_by(id=clue).first()
+
+        error = None
+        used_clues = [c.clue for c in puzzle.clues]
+        available_clues = [c for c in all_clues if c not in used_clues]
+        if clue is None:
+            error = "Clue does not exist"
+        elif not clue_fits(puzzle, clue, row, col, direction):
+            error = "Clue does not fit"
+        else:
+            puzzle_clue = PuzzleClue(row=row,col=col,direction=direction)
+            puzzle_clue.clue = clue
+            puzzle.clues.append(puzzle_clue)
+            available_clues.remove(clue)
+            self.session.flush()
+
+        self.render('edit_puzzle.html', puzzle=puzzle, clues=available_clues, error=error)
+
 
 class Application(tornado.web.Application):
     """The demo application."""
@@ -156,6 +256,8 @@ class Application(tornado.web.Application):
             (r'/auth/login', LoginHandler),
             (r'/auth/logout', LogoutHandler),
             (r'/clue/create', CreateClueHandler),
+            (r'/puzzle/create', CreatePuzzleHandler),
+            (r'/puzzle/edit/([^/]+)', EditPuzzleHandler),
         ]
         settings = {
             'template_path': path('templates'),

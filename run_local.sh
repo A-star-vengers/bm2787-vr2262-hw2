53c3c07ecb4c153a836730b233719b7ef032ef2e
Viktor Roytman <vr2262@columbia.edu>
2016-10-03 19:41:10 -0400

    Male run_local.sh a one-stop-shop

--- run_local.sh
+++ run_local.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
+if [ ! -d venv ]; then
+    echo "Creating virtual environment venv"
+    python3 -m venv venv
+fi
+echo "Entering virtual environment venv"
+source ./venv/bin/activate
+echo "Installing requirements if necessary..."
+pip install pip --upgrade > /dev/null
+pip install -r requirements.txt --upgrade > /dev/null
 if [ ! -f COOKIE_SECRET ]; then
+    echo "Creating COOKIE_SECRET file"
     head -c 24 /dev/urandom > COOKIE_SECRET
 fi
 ./demonstration.py

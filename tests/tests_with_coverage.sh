e263bab6d48698ac2a4925e6e84ab3ce147a95ed
Viktor Roytman <vr2262@columbia.edu>
2016-10-04 11:03:24 -0400

    Add coverage dependency to tests_with_coverage.sh

--- tests/tests_with_coverage.sh
+++ tests/tests_with_coverage.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+echo "Installing coverage if necessary..."
+pip install coverage --upgrade > /dev/null
 coverage erase
 coverage run --source=demo,demonstration.py --branch -m unittest ${@-discover tests}
 coverage html
